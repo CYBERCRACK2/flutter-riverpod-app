351b67057f4b44961fa42bbcbf866dfad99fc11f
CYBERCRACK2 <zhackperuca@gmail.com>
2026-03-02 22:33:13 -0400

    feat: refactor Pokemon information retrieval and update FamilyFutureScreen

--- lib/presentation/providers/05_future_provider/future_providers.dart
+++ lib/presentation/providers/05_future_provider/future_providers.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:riverpod_app/config/helpers/pokemon_information.dart';
 
@@ -9,6 +11,10 @@ class PokemonName extends _$PokemonName {
   Future<String> build() async {
     final pokemonId = ref.watch(pokemonIdProvider);
     final pokemonName = await PokemonInformation.getPokemonName(pokemonId);
+
+    // ref.onDispose(() {
+    //   debugPrint("raaa");
+    // })
 
     return pokemonName;
   }
@@ -22,4 +28,14 @@ class PokemonId extends _$PokemonId {
   void nextPokemon() {
     state++;
   }
+
+  void prevPokemon() {
+    state--;
+  }
+}
+
+@Riverpod(keepAlive: true)
+Future<String> pokemon(Ref ref, int pokemonId) async {
+  final pokemonName = await PokemonInformation.getPokemonName(pokemonId);
+  return pokemonName;
 }
--- lib/presentation/screens/06_family_future_provider/future_provider_screen.dart
+++ lib/presentation/screens/06_family_future_provider/future_provider_screen.dart
@@ -1,21 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/providers.dart';
 
-
-class FamilyFutureScreen extends StatelessWidget {
+class FamilyFutureScreen extends ConsumerStatefulWidget {
   const FamilyFutureScreen({super.key});
 
   @override
+  FamilyFutureScreenState createState() => FamilyFutureScreenState();
+}
+
+class FamilyFutureScreenState extends ConsumerState<FamilyFutureScreen> {
+  int pokemonId = 3;
+
+  @override
   Widget build(BuildContext context) {
+    final pokemonAsync = ref.watch(pokemonProvider(pokemonId));
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Family Future Provider'),
+        title: Text('Poquemon number $pokemonId'),
       ),
-      body: const Center(
-        child: Text('Fernando Herrera'),
+      body: Center(
+        child: pokemonAsync.when(
+            data: (data) => Text(data),
+            error: (error, stackTrace) => Text("$error"),
+            loading: () => const CircularProgressIndicator()),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon( Icons.refresh ),
-        onPressed: () {  },
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: "btn-1",
+            child: Icon(Icons.undo),
+            onPressed: () {
+              if (pokemonId <= 1) return;
+              pokemonId--;
+              setState(() {});
+            },
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.redo),
+            onPressed: () {
+              pokemonId++;
+              setState(() {});
+            },
+          ),
+        ],
       ),
     );
   }
