f9fece37115c7e4e533dd4532e31f84efbdbe830
CYBERCRACK2 <zhackperuca@gmail.com>
2026-03-02 23:09:33 -0400

    feat: add StreamProvider for user chat and update StreamProviderScreen

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -2,3 +2,4 @@ export '01_provider/hello_world_provider.dart';
 export '03_state_provider/state_provider.dart';
 export '04_todo_provider/todo_provider.dart';
 export '05_future_provider/future_providers.dart';
+export '07_stream_provider/stream_provider.dart';
--- lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
+++ lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/07_stream_provider/stream_provider.dart';
 
 class StreamProviderScreen extends StatelessWidget {
   const StreamProviderScreen({super.key});
@@ -10,6 +11,38 @@ class StreamProviderScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Stream Provider'),
       ),
+      body: StramView(),
     );
+  }
+}
+
+class StramView extends ConsumerWidget {
+  const StramView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final usersInChatAsync = ref.watch(userInChatProvider);
+
+    if (usersInChatAsync.isLoading) {
+      return const CircularProgressIndicator();
+    }
+
+    if (usersInChatAsync.hasError) {
+      return Center(
+        child: Text("${usersInChatAsync.error}"),
+      );
+    }
+
+    final user = usersInChatAsync.value!;
+
+    return ListView.builder(
+        itemCount: user.length,
+        itemBuilder: (BuildContext context, int index) {
+          final users = user[index];
+
+          return ListTile(
+            title: Text(users),
+          );
+        });
   }
 }
